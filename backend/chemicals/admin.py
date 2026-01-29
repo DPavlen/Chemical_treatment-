26910a6ef98e548c0d5978a0b206757e1eeb598e
Dmitry Pavlenko <125051658+DPavlen@users.noreply.github.com>
2026-01-29 19:31:17 +0400

    feat(chemicals): added model RequestLog & migrations, admin.

---
+++ backend/chemicals/admin.py
@@ -0,0 +1,103 @@
+from django.contrib import admin
+
+from .models import RequestLog
+
+
+@admin.register(RequestLog)
+class RequestLogAdmin(admin.ModelAdmin):
+    """Admin interface for RequestLog model."""
+
+    list_display = [
+        "id",
+        "user",
+        "ip_address",
+        "method",
+        "smiles_preview",
+        "image_format",
+        "success",
+        "response_time_ms",
+        "created_at",
+    ]
+    list_filter = [
+        "method",
+        "success",
+        "image_format",
+        "user",
+        "created_at",
+    ]
+    search_fields = [
+        "ip_address",
+        "smiles",
+        "user_agent",
+        "user__username",
+    ]
+    readonly_fields = [
+        "user",
+        "ip_address",
+        "method",
+        "smiles",
+        "has_molfile",
+        "width",
+        "height",
+        "image_format",
+        "success",
+        "error_message",
+        "response_time_ms",
+        "user_agent",
+        "created_at",
+    ]
+    fieldsets = (
+        (
+            "Request Info",
+            {
+                "fields": (
+                    "user",
+                    "ip_address",
+                    "method",
+                    "created_at",
+                ),
+            },
+        ),
+        (
+            "Chemical Data",
+            {
+                "fields": (
+                    "smiles",
+                    "has_molfile",
+                    "width",
+                    "height",
+                    "image_format",
+                ),
+            },
+        ),
+        (
+            "Result",
+            {
+                "fields": (
+                    "success",
+                    "error_message",
+                    "response_time_ms",
+                ),
+            },
+        ),
+        (
+            "Additional Info",
+            {
+                "fields": ("user_agent",),
+            },
+        ),
+    )
+    date_hierarchy = "created_at"
+    ordering = ["-created_at"]
+
+    def smiles_preview(self, obj):
+        """Show truncated SMILES string."""
+        if obj.smiles:
+            return obj.smiles[:30] + "..." if len(obj.smiles) > 30 else obj.smiles
+        return "-"
+
+    smiles_preview.short_description = "SMILES"
+
+    def has_add_permission(self, request):
+        """Disable manual creation of logs."""
+        return False
---
+++ backend/chemicals/models.py
@@ -0,0 +1,85 @@
+from django.conf import settings
+from django.db import models
+
+
+class RequestLog(models.Model):
+    """Log of API requests for analytics and monitoring."""
+
+    class Method(models.TextChoices):
+        GET = "GET", "GET"
+        POST = "POST", "POST"
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="request_logs",
+        verbose_name="User",
+    )
+    ip_address = models.GenericIPAddressField(
+        verbose_name="IP Address",
+        db_index=True,
+    )
+    method = models.CharField(
+        max_length=4,
+        choices=Method.choices,
+        verbose_name="HTTP Method",
+    )
+    smiles = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="SMILES String",
+    )
+    has_molfile = models.BooleanField(
+        default=False,
+        verbose_name="MOL File Uploaded",
+    )
+    width = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Image Width",
+    )
+    height = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Image Height",
+    )
+    image_format = models.CharField(
+        max_length=10,
+        blank=True,
+        verbose_name="Image Format",
+    )
+    success = models.BooleanField(
+        default=True,
+        verbose_name="Request Successful",
+    )
+    error_message = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Error Message",
+    )
+    response_time_ms = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Response Time (ms)",
+    )
+    user_agent = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="User Agent",
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Created At",
+        db_index=True,
+    )
+
+    class Meta:
+        verbose_name = "Request Log"
+        verbose_name_plural = "Request Logs"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        user_info = self.user.username if self.user else self.ip_address
+        return f"{self.method} {user_info} - {self.created_at:%Y-%m-%d %H:%M}"
